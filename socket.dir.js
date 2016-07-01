c33eecf1812bb049e44496acf8d3ec974d9debb9
Nick MacIntyre <nickmac@Nick-MacBook-Air.local>
2016-07-01 13:23:58 -0600

    set up filters and socket commands

--- socket.dir.js
+++ socket.dir.js
@@ -14,11 +14,29 @@
     }
 
     function controller ($scope) {
-      var socket = io.connect('http://localhost:3000');
+      var socket = io.connect('https://fathomless-falls-33454.herokuapp.com/');
       let vm = this
       vm.socketRoom = false
       vm.setRoom = setRoom;
       vm.sendSong = sendSong;
+      vm.state = {};
+
+      document.addEventListener('keydown', function (e){
+        var obj = {};
+        switch (e.which) {
+          case 39:
+            obj.command = 'next'
+            break;
+          case 37:
+            obj.command = 'back'
+            break;
+          case 32:
+            obj.command = 'space'
+            break;
+        }
+        obj.from = 'socket'
+        if(!!obj.command) socket.emit('server', {info: obj, room: socketRoom, to: 'electron'})
+      })
 
       function setRoom (data) {
         socketRoom = data
@@ -29,12 +47,16 @@
             vm.list = data;
             $scope.$apply()
           } else {
-            console.log(data);
+            vm.state = data
+            // console.log(vm.state.search.length === 1);
+            // if (vm.state.search.length === 1) vm.state.search = "*"
+            $scope.$apply();
           }
         })
       }
 
       function sendSong(path) {
+        // path.fill = !!path.fill ? path.fill : '';
         socket.emit('server', {to: 'electron', room: socketRoom, info: path})
       }
     }
